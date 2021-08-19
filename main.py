0a974da700b8f5aaee956ebae75f36255791ab86
Eric Born <eric.born85@gmail.com>
2021-08-18 19:28:17 -0500

    Create functions for start and stop relay

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 18 17:58:00 2021
+
+"""
+import RPi.GPIO as GPIO
 import time
 from datetime import datetime
 
 # input of persons time schedule
-persons_input = input('Please tell me what time?')
+#persons_input = input('Please tell me what time?')
 
+program_schedule = []
+
+# each zone 1-6 maps to GPIO port +1 of the zone number
+# 1 maps to 2, 2 to 3, etc.
+zone_dict = {1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 7}
+
+def start_relay(zone_number):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(zone_dict[zone_number])
+    GPIO.output(zone_dict[zone_number], True)
+    return(str(zone_dict[zone_number]) + ' is on')
+    
+def stop_relay(relay_number):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(relay_number)
+    GPIO.output(relay_number, False)
 
 # function to check persons input against current time
 # also adds to the time to create a window of time
@@ -27,8 +49,8 @@ def check_time(time):
         print('do nothing')
         time.sleep(5)
   
-# set loop to run continuously 
-running = True
+# # set loop to run continuously 
+# running = True
     
-while running:
-    check_time(persons_input)     
+# while running:
+#     check_time(persons_input)  
